a0d73f307b0657d7ce950c9144c349f74b952eb2
amit07bti <amitkumar.developer764@gmail.com>
2025-09-06 14:38:21 +0530

    handling both exception

--- src/exception_handling/CompileTimeException.java
+++ src/exception_handling/CompileTimeException.java
@@ -6,12 +6,16 @@ public class CompileTimeException {
     public static void main(String[] args) {
 
 
-        try{
-            FileInputStream  fis = new FileInputStream("D:\\xyz.txt.docx");
-            System.out.println("print:  "+fis);
-        }catch (Exception e){
-            System.out.println(e);
-        }
+
+//            FileInputStream  fis = new FileInputStream("D:\\xyz.txt.docx");
+//  todo  compile time exception--> complier will check at a compile and indicate please handle that exception .
+
+//             Class.forName("com.mysql.jdbc.Driver");
+
+
+        int a=100, b=0, c;      //todo runtime exception -> compiler will not able to check  while compile of program it will occures when run your program
+        c=a/b;
+        System.out.println(c);
 
     }
 }
