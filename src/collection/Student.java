be2cfb5a4fa489c13ffe5f425812023b1efbcba8
amit07bti <amitkumar.developer764@gmail.com>
2025-09-08 20:25:06 +0530

    return keyword

---
+++ src/collection/Student.java
@@ -0,0 +1,23 @@
+package collection;
+
+public class Student {
+
+    String name="Amit";
+    int age=12;
+
+    public static void main(String[] args) {
+        Student s1= new Student();
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println("-----------------------------------------------------------------");
+        Student s2= new Student();
+        System.out.println(s2.name);
+        System.out.println(s2.age);
+        System.out.println("-----------------------------------------------------------------");
+        Student s3= new Student();
+        System.out.println(s3.name);
+        System.out.println(s3.age);
+        System.out.println("-----------------------------------------------------------------");
+    }
+}
+
---
+++ src/variables/A.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class A {
+
+int a=10;
+
+
+
+}
+class B extends A{
+    int b=20;
+
+    void show(int a){
+        System.out.println(a);
+        System.out.println(this.a);
+        System.out.println(super.a);
+
+    }
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.show(30);
+
+    }
+}
